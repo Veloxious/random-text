ee6fb305ebd623947016071177a3e220feb387fb
Thomas Gonzales <thomas.j.gonzales@gmail.com>
2017-06-19 14:24:26 -0500

    hardmode done

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+var loremIpsum = require('lorem-ipsum')
 
-app.get('/hello', function(req, res){
-  res.send('Hello there!')
-});
+app.get('/lorem', function(req, res){
+  var output = loremIpsum({
+    count: 3
+   ,units: 'paragraphs'
+   ,format: 'html'
+  })
 
-app.get('/goodbye', function(req, res){
-  res.send('goodbye!')
-});
+  res.send(output)
+})
+
+app.get('/lorem/:paraNum', function(req, res){
+  var numPara = parseInt(req.params.paraNum)
+  var output = loremIpsum({
+    count: numPara
+   ,units: 'paragraphs'
+   ,format: 'html'
+  })
+
+  res.send(output)
+})
 
 app.listen(3000, function(){
   console.log('ojai, look at http://0.0.0.0:3000')
